62a20d1257a4a935d3060c64749c79f9fed9f4cc
KevinBasta <Kevin.basta22@ycdsbK12.ca>
2020-12-05 21:56:10 +0000

    added java docs

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -2,6 +2,13 @@ package recursionAssignment;
 
 public class Recursion{
 
+
+  /**
+  * Computes how many times the digit 7 is in the number given
+  *
+  * @param n  The number given
+  * @return The number of times the digit seven appears in the number
+  */
   public static int count7(int n) {
     int numbOfSeven = 0;
 
@@ -14,6 +21,14 @@ public class Recursion{
     return numbOfSeven + count7(n / 10);
   }
 
+
+
+  /**
+  * Computes 
+  *
+  * @param 
+  * @return 
+  */
   public static String pairStar(String str) {
     
     if (str.length() == 1 || str.equals("")) {
@@ -27,10 +42,20 @@ public class Recursion{
     }
   }
 
+
+
+  /**
+  * Computes if a number in the array given is followed by itself times 10
+  *
+  * @param intNums  The array of numbers
+  * @param index  The index number to start at in the array
+  * @return Boolean of weather or not a number in the array is followed by a number 10 times itself or not
+  */
   public static boolean array220(int[] intNums, int index) {
     if ((intNums.length - index) <= 1) {
       return false;
     }  
+    
     if (intNums[index] * 10 == intNums[index + 1]){
       return true;
     } else if (intNums.length > index){
@@ -40,4 +65,6 @@ public class Recursion{
       return false;
     }
   }
+
+
 }
